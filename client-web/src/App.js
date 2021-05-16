bac4aa53b32f29fc183e5cdf05eb471a0b949f48
abhaydee <abhaygirish1997@gmail.com>
2021-05-16 20:27:51 +0530

    initializing different screens

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+    </Router>
+  );
 }
 
 export default App;
--- client-web/src/index.js
+++ client-web/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:8000/graphql",
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
