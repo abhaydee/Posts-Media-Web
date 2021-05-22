c0384990a6e69b7d72216f59f224dd3af4a9687a
abhaydee <abhaygirish1997@gmail.com>
2021-05-22 12:12:24 +0530

    changes to link

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
 import "./App.css";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
+import MenuBar from "./Components/Menubar";
 import Register from "./Components/Register";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Container>
     </Router>
   );
 }
