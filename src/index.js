1760a05a8b2e487651423223cea1d17ff5c424e7
abhaydee <abhaygirish1997@gmail.com>
2021-05-30 16:57:24 +0530

    base url changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 const httpLink = createHttpLink({
-  uri: "http://localhost:8000/graphql",
+  uri: "https://shrouded-refuge-55173.herokuapp.com/",
 });
 const authorizationLink = setContext(({ _, headers }) => {
   const token = localStorage.getItem("jwtToken");
