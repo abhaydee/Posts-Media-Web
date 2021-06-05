5f73fb0076a25fb475dd50ff34443c75e34e5d7b
abhaydee <abhaygirish1997@gmail.com>
2021-06-05 13:40:38 +0530

    changes to env

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: require("./env") });
 module.exports = {
-  MONGODB: process.env.mongodbUrl,
-  secretKey: process.env.secretKeyToken,
+  MONGODB: process?.env?.mongodbUrl,
+  secretKey: process?.env?.secretKeyToken,
 };
