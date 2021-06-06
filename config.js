63a8e78ffebbec8e20a5e288f560c535a710433e
abhaydee <abhaygirish1997@gmail.com>
2021-06-06 11:52:48 +0530

    removing env config

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 module.exports = {
   MONGODB: `mongodb+srv://${process?.env?.mongodbUsername}:${process?.env?.password}@test-cluster.qcll2.mongodb.net/merng?retryWrites=true&w=majority`,
   secretKey: process?.env?.secretToken,
