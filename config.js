8397ce8d0314111d84b170e3c17be39309f42a65
abhaydee <abhaygirish1997@gmail.com>
2021-06-14 19:04:00 +0530

    changes to mongodb

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB: `mongodb+srv://${process?.env?.mongodbUsername}:${process?.env?.password}@test-cluster.qcll2.mongodb.net/merng?retryWrites=true&w=majority`,
-  secretKey: process?.env?.secretToken,
+  MONGODB: `mongodb+srv://abhaydee:hpau003tx@test-cluster.qcll2.mongodb.net/merng?retryWrites=true&w=majority`,
+  secretKey: "some-secret-key",
 };
